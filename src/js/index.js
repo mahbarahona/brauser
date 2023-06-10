a4b49996c4f0c1f82a318b423941806fbadc37a2
Manuel Hernandez <mahbarahona@gmail.com>
2023-06-09 23:01:47 -0400

    baseline

--- src/js/index.js
+++ src/js/index.js
@@ -26,37 +26,15 @@ const whenFileLoaded = async (event) => {
     appState.history.push(newUI);
     // render
     const currentUI = appState.currentUI.screen;
-    const renderer = new Renderer();
+    const engine = document.querySelector('#engine');
+    const renderer = new Renderer(engine);
     renderer.draw(currentUI);
   } catch (error) {
     console.warn({ message: 'Unable to read file', error });
   }
 };
 
-const whenSearch = async (event) => {
-  event.preventDefault();
-  const q = event.target.search.value;
-  //empty query
-  if (!q) return;
-
-  const urlPattern =
-    /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
-
-  const isUrl = new RegExp(urlPattern).test(q);
-
-  if (isUrl) {
-    console.log('got to address', { q });
-    console.warn('Not implemented');
-  } else {
-    console.log('got to our db', { q });
-    console.warn('Not implemented');
-  }
-};
-
 document.addEventListener('DOMContentLoaded', () => {
   const fileInput = document.querySelector('#file-input');
   fileInput.addEventListener('change', whenFileLoaded);
-
-  const searchForm = document.querySelector('#search-form');
-  searchForm.addEventListener('submit', whenSearch);
 });
--- src/js/modules/renderer.js
+++ src/js/modules/renderer.js
@@ -3,30 +3,117 @@ import { Processor, ProcessorStep } from '../models/processor';
 class RendererStep_Layout extends ProcessorStep {
   handle(data) {
     console.log('renderer:layout', data);
+    // calc sizes
+    data.elements = data.elements.map((el) => new UIControl({ el }));
     return data;
   }
 }
 class RendererStep_Painting extends ProcessorStep {
-  handle(data) {
-    console.log('renderer:painting', data);
-    return data;
+  // eslint-disable-next-line constructor-super
+  constructor(engine, context) {
+    super();
+    this.engine = engine;
+    this.ctx = context;
+  }
+  handle(ui) {
+    console.log('renderer:painting', ui);
+    // draw elements
+    this.ctx.fillStyle = '#ccc';
+    this.ctx.fillRect(0, 0, ui.width, ui.height);
+    ui.elements.forEach((el) => el.draw(this.ctx));
+    return ui;
   }
 }
 class RendererStep_Composite extends ProcessorStep {
   handle(data) {
     console.log('renderer:composite', data);
+    // merge and output image
     return data;
   }
 }
 
 export class Renderer {
-  constructor() {}
-  draw(elements) {
-    const drawProcess = new Processor([
+  constructor(engine) {
+    if (!engine) throw new Error('engine not available');
+    this.engine = engine;
+    //
+    this.engine.width = 300;
+    this.engine.height = 400;
+    //
+    this.ctx = this.engine.getContext('2d');
+    this.drawProcess = new Processor([
       new RendererStep_Layout(),
-      new RendererStep_Painting(),
+      new RendererStep_Painting(this.engine, this.ctx),
       new RendererStep_Composite()
     ]);
-    return drawProcess.run(elements);
+  }
+  draw(elements) {
+    return this.drawProcess.run(elements);
+  }
+}
+
+class UIControl {
+  constructor({ el, config }) {
+    this.el = el;
+    this.config = config;
+    this.el.type = this.el.type.toLowerCase();
+    this.controls = {
+      text: ({ ctx, config }) => {
+        ctx.beginPath();
+        ctx.fillStyle = 'green';
+        ctx.rect(10, 10, 50, 50);
+        ctx.stroke();
+      },
+      arc: ({ ctx, config }) => {
+        ctx.beginPath();
+        ctx.fillStyle = 'blue';
+        ctx.rect(100, 100, 50, 50);
+        ctx.fill();
+        ctx.stroke();
+      },
+      dot: ({ ctx, config }) => {
+        ctx.beginPath();
+        ctx.fillStyle = 'red';
+        ctx.rect(150, 150, 50, 50);
+        ctx.fill();
+        ctx.stroke();
+      },
+      rect: ({ ctx, config }) => {
+        ctx.beginPath();
+        ctx.fillStyle = 'purple';
+        ctx.rect(200, 200, 50, 50);
+        ctx.fill();
+        ctx.stroke();
+      },
+      polygon: ({ ctx, config }) => {
+        ctx.beginPath();
+        ctx.fillStyle = 'fucsia';
+        ctx.rect(10, 300, 50, 50);
+        ctx.fill();
+        ctx.stroke();
+      },
+      line({ ctx, config }) {
+        const { from, to } = {
+          from: { x: 10, y: 20 },
+          to: { x: 50, y: 50 }
+        };
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 3;
+
+        ctx.beginPath();
+        ctx.moveTo(from.x, from.y);
+        ctx.lineTo(to.x, to.y);
+        ctx.stroke();
+      }
+    };
+  }
+  draw(context) {
+    // utils.info(this);
+    try {
+      this.controls[this.el.type]({ ctx: context, config: this.config });
+    } catch (error) {
+      console.log({ error });
+      // utils.track({ key: 'renderer--controls--error', val: this });
+    }
   }
 }
